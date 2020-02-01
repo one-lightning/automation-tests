6234298bb1132ee5f3f5fc23e10aa535c30584a8
onelightning <lightning@thunderstorm.pro>
2020-02-01 14:01:30 +0200

    [SetUp_WebDriver]

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,24 +1,22 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BaseTests {
+public class BaseTest {
 
 	private WebDriver driver;
 
-	
 	public void RunTest() {
 		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://the-internet.herokuapp.com/");
+		driver.get("https://google.com/");
 		System.out.println(driver.getTitle());
-		driver.quit();
+		driver.close();
 	}
 
 	public static void main(String[] args) {
-		BaseTests tests = new BaseTests();
+		BaseTest tests = new BaseTest();
+		tests.RunTest();
 	}
 }
