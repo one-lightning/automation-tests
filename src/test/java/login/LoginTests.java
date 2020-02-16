7201b5a42b5bab45bce5ccd412c58b3fb166660e
onelightning <lightning@thunderstorm.pro>
2020-02-16 17:29:50 +0200

    [LoginTests.java]

---
+++ src/test/java/login/LoginTests.java
@@ -0,0 +1,23 @@
+package login;
+
+import base.BaseTests;
+import junit.framework.Assert;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+public class LoginTests extends BaseTests {
+
+	public void testSuccessfulLogin() {
+		HomePage homePage = new HomePage(driver);
+
+		LoginPage loginPage = homePage.clickFormAuthentication();
+
+		loginPage.setUsername("tomsmith");
+		loginPage.setPassword("SuperSecretPassword!");
+
+		SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+		Assert.assertEquals(secureAreaPage.getAlertText(), "You logged into a secure area! x",
+				"Alert text is incorrect");
+	}
+}
